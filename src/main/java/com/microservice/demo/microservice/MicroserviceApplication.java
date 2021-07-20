ba935dde52f5556da40d3bfea67cdba91dfd1476
vetrivetri <51082669+vetrivetri@users.noreply.github.com>
2021-07-20 10:46:33 +0530

    INITIAL COMMIT

---
+++ src/main/java/com/microservice/demo/microservice/MicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.microservice.demo.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@SpringBootApplication
+@EnableEurekaServer
+public class MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApplication.class, args);
+	}
+
+}
