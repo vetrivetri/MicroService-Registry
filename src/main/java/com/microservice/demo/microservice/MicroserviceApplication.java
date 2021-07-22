d91f86f5ddde6b02facdbf2db955f833821b4d8f
vetrivetri <51082669+vetrivetri@users.noreply.github.com>
2021-07-22 20:52:25 +0530

    LOG4j CHANGES

--- src/main/java/com/microservice/demo/microservice/MicroserviceApplication.java
+++ src/main/java/com/microservice/demo/microservice/MicroserviceApplication.java
@@ -1,5 +1,7 @@
 package com.microservice.demo.microservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -7,7 +9,14 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class MicroserviceApplication {
 
+
 	public static void main(String[] args) {
+
+		Logger logger = LoggerFactory.getLogger(MicroserviceApplication.class);
+		logger.info("This is Info Log");
+		logger.debug("This is Debug Log");
+		logger.trace("This is Trace Log");
+		logger.warn("This is warn log");
 		SpringApplication.run(MicroserviceApplication.class, args);
 	}
 
